0a531ec802f99b30cbc443b38fa93406ce85814c
Peter Khalil <peter.khalil.fahmy@gmail.com>
2024-08-11 02:12:41 +0300

    🐛  Updating middleware to handle redirect for root path

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,20 +1,26 @@
 import createMiddleware from 'next-intl/middleware';
 import { NextRequest, NextResponse } from 'next/server';
 
-export default createMiddleware({
-  locales: ['en', 'ar'],
-  defaultLocale: 'en',
-});
-
 export function middleware(request: NextRequest) {
   const url = request.nextUrl.clone();
+  const pathname = new URL(url.toString()).pathname;
   request.headers.set('x-url', url.toString());
+
+  if (pathname === '/') {
+    return NextResponse.redirect(url.toString() + 'en');
+  }
+
   return NextResponse.next({
     request: {
       headers: request.headers,
     },
   });
 }
+
+export default createMiddleware({
+  locales: ['en', 'ar'],
+  defaultLocale: 'en',
+});
 
 export const config = {
   matcher: ['/', '/(ar|en)/:path*'],
